939f06479716791bacbb1e436eb65a84f3de4c0f
neg2led <4232981+neg2led@users.noreply.github.com>
2020-11-04 19:06:04 +1100

    update scripts to account for opt/ toolchain

--- env.sh
+++ env.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# save current PATH
-#OLDPATH=$PATH
-#export PATH=$PATH:$(pwd)/arm-2014.05/bin
-
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-2014.05/bin/arm-none-linux-gnueabi-
+export CROSS_COMPILE=/opt/arm-2014.05/bin/arm-none-linux-gnueabi-
 export TYPE=xc
 export IPV=420.69 # ha ha, yes, very funny, self
